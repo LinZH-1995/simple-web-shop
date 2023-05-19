d226531ad9d95825c19827529b6184c3f829c92e
LinZH-1995 <lin84610@gmail.com>
2023-05-19 21:43:12 +0800

    feat: add POST order、POST cancelOrder

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,4 @@
-const { Order, Product } = require('../models')
+const { Order, Product, OrderItem, Cart } = require('../models')
 
 const orderController = {
   getOrders: async (req, res, next) => {
@@ -10,8 +10,46 @@ const orderController = {
         ]
       })
       const ordersData = orders.map(order => order.toJSON())
-      console.log(ordersData[1].items)
       res.render('orders', { orders: ordersData })
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  postOrder: async (req, res, next) => {
+    try {
+      const cartId = req.body.cartId
+      const userId = req.user?.id
+      delete req.body.cartId
+      const [order, cart] = await Promise.all([
+        Order.create(Object.assign(req.body, { userId })),
+        Cart.findByPk(cartId, { nest: true, include: [{ model: Product, as: 'items', attributes: ['id', 'price'], through: { attributes: ['quantity'] } }] })
+      ])
+      await Promise.all(Array.from({ length: cart.items.length }, (e, i) => {
+        return OrderItem.create({
+          price: cart.items[i].price,
+          quantity: cart.items[i].CartItem.quantity,
+          orderId: order.id,
+          productId: cart.items[i].id
+        })
+      }))
+      res.redirect('/orders')
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  cancelOrder: async (req, res, next) => {
+    try {
+      const id = req.params.id
+      const [order, orderItems] = await Promise.all([
+        Order.findByPk(id),
+        OrderItem.findAll({ where: { orderId: id } })
+      ])
+      await Promise.all(Array.from({ length: orderItems.length }, (e, i) => orderItems[i].destroy()))
+      await order.destroy()
+      console.log(order, '-------', orderItems)
+      res.redirect('back')
     } catch (err) {
       next(err)
     }
--- routes/modules/orders.js
+++ routes/modules/orders.js
@@ -3,6 +3,10 @@ const router = express.Router()
 
 const orderController = require('../../controllers/orderController.js')
 
+router.post('/:id/cancel', orderController.cancelOrder)
+
+router.post('/', orderController.postOrder)
+
 router.get('/', orderController.getOrders)
 
 module.exports = router
